ccd17b276df61b9b2ba2ff64ff767e29fd953392
diyorbekYusupov <dyuzb2004@gmail.com>
2022-02-23 15:11:23 +0500

    refactored

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
 window.addEventListener("DOMContentLoaded", () => {
-	const searchBtn = document.querySelector("#search-btn");
-	const searchInputValue = document.querySelector("#search-input");
-	const errorBox = document.querySelector("#error-alert");
-	function getLocationInfo(cityName) {
-	 	navigator.geolocation.getCurrentPosition((position) => {
-			let query;
-			let latitude = position.coords.latitude;
-			let longitude = position.coords.longitude;
-			if( cityName === undefined ) {
-				query = `${latitude},${longitude}`
-			}else {
-				query = cityName;
-			}
-		 	fetch(
-				`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${query}&days=10`,
-				{
-					method: "GET",
-					headers: {
-						"x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-						"x-rapidapi-key":
-							"883222602dmsh572987bdcdee66cp1cac6ejsnb9bf2f5ea3e9",
-					},
-				}
-			)
-			.then((response) => response.json())
-			.then((response) => showInfo(response))
-			.catch((err) => {
-				errorHandler();
-			});
+    const searchBtn = document.querySelector("#search-btn");
+    const searchInputValue = document.querySelector("#search-input");
+    const errorBox = document.querySelector("#error-alert");
+    function getLocationInfo(cityName) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let query;
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            if (cityName === undefined) {
+                query = `${latitude},${longitude}`;
+            } else {
+                query = cityName;
+            }
+            fetch(
+                `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${query}&days=10`,
+                {
+                    method: "GET",
+                    headers: {
+                        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+                        "x-rapidapi-key":
+                            "883222602dmsh572987bdcdee66cp1cac6ejsnb9bf2f5ea3e9",
+                    },
+                }
+            )
+                .then((response) => response.json())
+                .then((response) => showInfo(response))
+                .catch((err) => {
+                    errorHandler();
+                });
+        });
+    }
+    getLocationInfo();
+    function showInfo(response) {
+        document.querySelector("#country").innerHTML =
+            response.location.country;
+        document.querySelector("#region").innerHTML = response.location.region;
+        document.querySelector("#localTime").innerHTML =
+            response.location.localtime.slice(0, 10);
+        document.querySelector("#current-temp").innerHTML =
+            response.current.temp_c + " C°";
+        document.querySelector("#current-condition").innerHTML =
+            response.current.condition.text;
+        document.querySelector("#current-humidity").innerHTML =
+            "Humidity: " + response.current.humidity + " %";
+        document.querySelector("#wind-direction").innerHTML =
+            "Wind direction: " + response.current.wind_dir;
+        document.querySelector("#wind-speed").innerHTML =
+            "Wind speed: " + response.current.wind_kph + "KM/H";
+        document.querySelector("#current-condition-img").src =
+            response.current.condition.icon;
 
-		});
-	}
-	getLocationInfo();
-	function showInfo(response) {
-		document.querySelector("#country").innerHTML = response.location.country;
-		document.querySelector("#region").innerHTML = response.location.region;
-		document.querySelector("#localTime").innerHTML = response.location.localtime.slice(0,10);
-		document.querySelector("#current-temp").innerHTML = response.current.temp_c+" C°";
-		document.querySelector("#current-condition").innerHTML = response.current.condition.text;
-		document.querySelector("#current-humidity").innerHTML = "Humidity: "+response.current.humidity+" %";
-		document.querySelector("#wind-direction").innerHTML = "Wind direction: "+response.current.wind_dir;
-		document.querySelector("#wind-speed").innerHTML = "Wind speed: "+response.current.wind_kph+"KM/H";
-		document.querySelector("#current-condition-img").src = response.current.condition.icon;
-
-		const dailyForecastBox = document.querySelector("#daily-forecast");
-		dailyForecastBox.innerHTML = `
+        const dailyForecastBox = document.querySelector("#daily-forecast");
+        dailyForecastBox.innerHTML = `
 		<p class="text-center">Daily Forecast</p>
 		<div>
 			<h3 class="text-center">${response.forecast.forecastday[1].date}</h3>
@@ -78,10 +85,12 @@ window.addEventListener("DOMContentLoaded", () => {
 		</div>
 		`;
 
-		const hourlyForecastBox = document.querySelector("#hourly-forecast-container");
-		let hourlyContent = "";
-		response.forecast.forecastday[0].hour.forEach((item) => {
-			hourlyContent += `
+        const hourlyForecastBox = document.querySelector(
+            "#hourly-forecast-container"
+        );
+        let hourlyContent = "";
+        response.forecast.forecastday[0].hour.forEach((item) => {
+            hourlyContent += `
 			<div id="forecast-item" class="col">
 				<p class="text-center">${item.time.slice(10)}</p>
 				<div class="d-flex justify-content-between">
@@ -97,19 +106,18 @@ window.addEventListener("DOMContentLoaded", () => {
 				</div>
 			</div>
 			`;
-		});
-		hourlyForecastBox.innerHTML = hourlyContent
-	} 
-	searchBtn.addEventListener("click", () => {
-		getLocationInfo(searchInputValue.value);
-	});
-	searchInputValue.addEventListener("input", () => {
-	});
+        });
+        hourlyForecastBox.innerHTML = hourlyContent;
+    }
+    searchBtn.addEventListener("click", () => {
+        getLocationInfo(searchInputValue.value);
+    });
+    searchInputValue.addEventListener("input", () => {});
 
-	function errorHandler() {
-		errorBox.style.opacity = 1;
-		setTimeout(() => {
-			errorBox.style.opacity = 0;
-		},5000)
-	}
+    function errorHandler() {
+        errorBox.style.opacity = 1;
+        setTimeout(() => {
+            errorBox.style.opacity = 0;
+        }, 5000);
+    }
 });
